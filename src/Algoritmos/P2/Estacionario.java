fbf443c6332f259424e3d5ccec53e2935fddd5ac
Grupo2 - Pierre-Simon Callist YannickTondreau <pst00006@red.ujaen.es>
2017-11-10 10:15:44 +0100

    Greedy Inicial

--- src/Algoritmos/P2/Estacionario.java
+++ src/Algoritmos/P2/Estacionario.java
@@ -5,10 +5,126 @@
  */
 package Algoritmos.P2;
 
+import Utils.Restricciones;
+import static Utils.Utilidades.*;
+import Utils.listaTransmisores;
+import Utils.rangoFrec;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import static main.main.NUMERO;
+
 /**
  *
  * @author ptondreau
  */
 public class Estacionario {
     
+    List<List<Integer>> frecuencias = new ArrayList<> ();
+    List<Integer> transmisores = new ArrayList<> ();
+    List<Integer> frecuenciasR = new ArrayList<>();
+    Restricciones restricciones;
+    int []resultado = new int [50];
+    
+    List<List<Integer>> padres = new ArrayList<> ();
+    List<List<Integer>> hijos = new ArrayList<> ();
+    
+    public Estacionario(listaTransmisores _transmisores, rangoFrec _frecuencias, Restricciones _rest ) throws FileNotFoundException {
+        frecuencias = _frecuencias.rangoFrecuencias;
+        transmisores = _transmisores.transmisores;
+        restricciones = _rest;
+        
+        for ( int i = 0; i < 50; i++ ){
+            padres.add(new ArrayList<>());
+        }
+        
+        for ( int i = 0; i < 50; i++ ) {
+            greedyInicial(i);
+        }
+    }
+    
+    void greedyInicial ( int id ) throws FileNotFoundException {
+                
+        for ( int i = 0; i < transmisores.size(); i++ ) {
+            frecuenciasR.add(0);
+        }
+
+        Random numero = NUMERO;
+        int seleccionado = numero.nextInt(transmisores.size());
+        
+        int tamanio = frecuencias.get(transmisores.get(seleccionado)).size();
+        int frecuenciaRandom = frecuencias.get(transmisores.get(seleccionado)).get(numero.nextInt(tamanio));
+        frecuenciasR.set(seleccionado, frecuenciaRandom);
+        
+        System.out.println("Transmisor seleccionado: "+seleccionado);
+        
+        List<List<Integer>> listaRestric = new ArrayList<>();
+        int transmisor = 0;
+        boolean fin = false;
+        while( transmisor < transmisores.size()) {
+            listaRestric = restricciones.restriccionesTransmisor(transmisor);
+            if (  transmisor != seleccionado && listaRestric.size() > 0 ) {
+
+                int minimo = Integer.MAX_VALUE;
+                boolean encontrado = false;
+                int frecuenciaR = 0;
+                int frecuencia;
+                int pos = 0;
+
+                int valor = 0; //Sacado del bucle while
+
+                while( pos < frecuencias.get(transmisores.get(transmisor)).size() &&  ! encontrado ) {
+
+                    List<Integer> nuevaLista = new ArrayList<>();
+                    nuevaLista.addAll(frecuenciasR);
+
+                    frecuencia = frecuencias.get(transmisores.get(transmisor)).get(pos);
+                    nuevaLista.set(transmisor, frecuencia);
+                    List<List<Integer>> listaRest = compruebaTransmisores(transmisor, restricciones, frecuenciasR);
+
+                    if ( listaRest.size() > 0 ) { // Lista no vacía, se selecciona frecuencia que afecte lo menos posible al resultado
+
+                        valor = rDiferencia(nuevaLista, listaRest);
+                        if ( valor < minimo ) {
+                            minimo = valor;
+                            frecuenciaR = frecuencia;
+                            if ( valor == 0 ) // Si la suma de todas las restricciones = 0 entonces es el mejor resultado posible
+                            {
+                                encontrado = true;
+                            }
+                        }
+                    } else { // En caso de que la lista este vacía no hay restricciones que se puedan satisfacer -> frecuencia aleatoria
+
+                        tamanio = frecuencias.get(transmisores.get(transmisor)).size();
+                        frecuenciaR = frecuencias.get(transmisores.get(transmisor)).get(numero.nextInt(tamanio));
+                        valor = 0;
+                        encontrado = true;
+                    }
+                    pos ++;
+                }
+                frecuenciasR.set(transmisor, frecuenciaR);
+            }
+            transmisor++;
+        }
+        resultado[id] = rDiferencia(frecuenciasR, restricciones);
+        padres.get(id).addAll(frecuenciasR);
+        frecuenciasR.clear(); // Borra todos los elementos anteriores para nueva solucion
+    }
+    
+    void generarHijos () {
+        
+    }
+    
+    public void resultados () {
+
+        List<List<Integer>> listaTrans = new ArrayList<>();
+        for ( int i = 0; i < transmisores.size(); i ++ ) {
+            listaTrans = restricciones.restriccionesTransmisor(i);
+            if ( listaTrans.size() > 0 ) {
+                System.out.println("Transmisor " + (i + 1) + ": " + frecuenciasR.get(i));
+            }
+        }
+        System.out.println("Coste: " + resultado);
+    }
 }
